0f11fe9750b58cb6251993104f12183c42077b2a
Martin Habovštiak <martin.habovstiak@gmail.com>
2015-03-04 18:19:09 +0100

    Improved code, added features

--- ProgressbarControl.ino
+++ ProgressbarControl.ino
@@ -35,6 +35,7 @@ EthernetServer server(80);
 // ======== Code ========
 
 typedef void (*StaticSender)(EthernetClient &);
+typedef void (*ParamSender)(EthernetClient &, const char *);
 
 // Holds information about static resource
 class StaticResource {
@@ -55,6 +56,65 @@ class StaticResource {
 		const char *mPath;
 		StaticSender mSender;
 };
+
+// Holds information about parametrized resource
+class ParamResource {
+	public:
+		// Constructor just stores the pointers
+		ParamResource(const char *prefix, ParamSender rs) :
+			mPrefix(prefix),
+			mSender(rs) {
+		}
+
+		// Checks whether path matches and calls the ParamSender if it does
+		bool match(const char *path, EthernetClient &client) {
+			size_t i = 0;
+			for(; mPrefix[i] && path[i]; ++i) {
+				if(mPrefix[i] != path[i])
+					return false;
+			}
+
+			if(mPrefix[i]) return false;
+
+			mSender(client, path + i);
+			return true;
+		}
+	private:
+		const char *mPrefix;
+		ParamSender mSender;
+};
+
+// Maps pin number to string
+class PinMap {
+	public:
+		PinMap(const char *prefix, int pin) :
+			mPrefix(prefix),
+			mPin(pin) {
+		}
+
+		// If prefix matches, fills pin and tail variables and returns true
+		bool match(const char *path, int &pin, const char *&tail) {
+			size_t i = 0;
+			for(; mPrefix[i] && path[i]; ++i) {
+				if(mPrefix[i] != path[i])
+					return false;
+			}
+
+			if(mPrefix[i]) return false;
+
+			pin = mPin;
+			tail = path + i;
+			return true;
+		}
+	private:
+		const char *mPrefix;
+		int mPin;
+};
+
+// Timer variables
+unsigned long countdownStart;
+unsigned long countdownMillis;
+bool countdownTimer = false;
 
 // Last states of the switches
 bool lastMain;
@@ -155,18 +215,18 @@ void sendIndex(EthernetClient &client) {
 			"<body>"
 			"<h1>Progressbar control panel</h1>"
 			"<h2>Lights</h2>"
-			"Front <a href=\"lights/mainFrontOn\">on</a>|<a href=\"lights/mainFrontOff\">off</a><br>"
-			"Back <a href=\"lights/mainBackOn\">on</a>|<a href=\"lights/mainBackOff\">off</a><br>"
-			"Left <a href=\"lights/labLeftOn\">on</a>|<a href=\"lights/labLeftOff\">off</a><br>"
-			"Right <a href=\"lights/labRightOn\">on</a>|<a href=\"lights/labRightOff\">off</a><br>"
+			"Front <a href=\"lights/main/front/On\">on</a>|<a href=\"lights/main/front/Off\">off</a><br>"
+			"Back <a href=\"lights/main/back/On\">on</a>|<a href=\"lights/main/back/Off\">off</a><br>"
+			"Left <a href=\"lights/lab/left/On\">on</a>|<a href=\"lights/lab/left/Off\">off</a><br>"
+			"Right <a href=\"lights/lab/right/On\">on</a>|<a href=\"lights/lab/right/Off\">off</a><br>"
 			"<h2>Sockets</h2>"
-			"A <a href=\"sockets/AOn\">on</a>|<a href=\"sockets/AOff\">off</a><br>"
-			"B <a href=\"sockets/BOn\">on</a>|<a href=\"sockets/BOff\">off</a><br>"
-			"C <a href=\"sockets/COn\">on</a>|<a href=\"sockets/COff\">off</a><br>"
-			"D <a href=\"sockets/DOn\">on</a>|<a href=\"sockets/DOff\">off</a><br>"
-			"E <a href=\"sockets/EOn\">on</a>|<a href=\"sockets/EOff\">off</a><br>"
+			"A <a href=\"sockets/A/On\">on</a>|<a href=\"sockets/A/Off\">off</a><br>"
+			"B <a href=\"sockets/B/On\">on</a>|<a href=\"sockets/B/Off\">off</a><br>"
+			"C <a href=\"sockets/C/On\">on</a>|<a href=\"sockets/C/Off\">off</a><br>"
+			"D <a href=\"sockets/D/On\">on</a>|<a href=\"sockets/D/Off\">off</a><br>"
+			"E <a href=\"sockets/E/On\">on</a>|<a href=\"sockets/E/Off\">off</a><br>"
 			"<h2>The red button</h2>"
-			"<a href=\"everythingOff\">Shutdown the Universe</a><br>"
+			"Shutdown the Universe <a href=\"everythingOff\">now</a> | <a href=\"everythingOff/60\">after one minute</a> | <a href=\"everythingOff/300\">after five minutes</a><br>"
 			"</body>"
 		"</html>"
 	};
@@ -174,103 +234,24 @@ void sendIndex(EthernetClient &client) {
 	sendPMString(client, (uint_farptr_t)index);
 }
 
-// Lights
-// TODO: replace static resources with something more inteligent
-void mainFrontOn(EthernetClient &client) {
-	redirect(client);
-	digitalWrite(mainLightFront, HIGH);
-}
+// Parses int in str, returns last position (0 means invalid char)
+size_t parseInt(const char *str, uint16_t &num) {
+	size_t i = 0;
+	num = 0;
 
-void mainFrontOff(EthernetClient &client) {
-	redirect(client);
-	digitalWrite(mainLightFront, LOW);
-}
+	for(; str[i] != '/' && str[i] != 0; ++i) {
+		if(str[i] < '0' || str[i] > '9') {
+			return 0;
+		}
+		num *= 10;
+		num += str[i] - '0';
+	}
 
-void mainBackOn(EthernetClient &client) {
-	redirect(client);
-	digitalWrite(mainLightBack, HIGH);
-}
-
-void mainBackOff(EthernetClient &client) {
-	redirect(client);
-	digitalWrite(mainLightBack, LOW);
-}
-
-void labLeftOn(EthernetClient &client) {
-	redirect(client);
-	digitalWrite(labLightLeft, HIGH);
-}
-
-void labLeftOff(EthernetClient &client) {
-	redirect(client);
-	digitalWrite(labLightLeft, LOW);
-}
-
-void labRightOn(EthernetClient &client) {
-	redirect(client);
-	digitalWrite(labLightRight, HIGH);
-}
-
-void labRightOff(EthernetClient &client) {
-	redirect(client);
-	digitalWrite(labLightRight, LOW);
-}
-
-// Sockets
-void socketAOn(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'A', 1);
-}
-
-void socketBOn(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'B', 1);
-}
-
-void socketCOn(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'C', 1);
-}
-
-void socketDOn(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'D', 1);
-}
-
-void socketEOn(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'E', 1);
-}
-
-void socketAOff(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'A', 0);
-}
-
-void socketBOff(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'B', 0);
-}
-
-void socketCOff(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'C', 0);
-}
-
-void socketDOff(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'D', 0);
-}
-
-void socketEOff(EthernetClient &client) {
-	redirect(client);
-	solight.control(31, 'E', 0);
+	return i;
 }
 
 // Kill switch
-// TODO: add timer support
-void everythingOff(EthernetClient &client) {
-	redirect(client);
+void turnEverythingOff() {
 	digitalWrite(mainLightFront, HIGH);
 	digitalWrite(mainLightFront, LOW);
 	digitalWrite(mainLightBack, HIGH);
@@ -286,31 +267,137 @@ void everythingOff(EthernetClient &client) {
 	solight.control(31, 'E', 0);
 }
 
-#define LI(NAME) StaticResource("/lights/" #NAME, &NAME)
-#define SI(NAME) StaticResource("/sockets/" #NAME, &socket ## NAME)
+void everythingOff(EthernetClient &client) {
+	turnEverythingOff();
+	redirect(client);
+}
 
-// TODO: some sort of automatic table generation?
+void everythingOffTimer(EthernetClient &client, const char *param) {
+	uint16_t seconds;
+	size_t i = parseInt(param, seconds);
+
+	if(!i || param[i]) {
+		send404(client);
+		return;
+	}
+
+	countdownStart = millis();
+	countdownMillis = seconds * 1000;
+	countdownTimer = true;
+
+	static const char head[] PROGMEM = {
+		"<html>"
+			"<head>"
+				"<title>Countdown</title>"
+			"</head>"
+			"<body>"
+				"<h1>The Universe will shut down in <span id=\"secs\"></span> seconds</h1>"
+				"<script type=\"text/javascript\">"
+	};
+	static const char tail[] PROGMEM = {
+					"function tmr() {"
+						"if(secs) --secs;"
+						"document.getElementById(\"secs\").innerHTML = secs;"
+					"}"
+
+					"document.getElementById(\"secs\").innerHTML = secs;"
+					"window.setInterval(tmr, 1000);"
+				"</script>"
+			"</body>"
+		"</html>"
+	};
+
+	sendPMString(client, (uint_farptr_t)head);
+	client.print("secs = ");
+	client.print(param);
+	client.print(";");
+	sendPMString(client, (uint_farptr_t)tail);
+}
+
+void lights(EthernetClient &client, const char *param) {
+	static PinMap pins[] = {
+		PinMap("main/front/", mainLightFront),
+		PinMap("main/back/", mainLightBack),
+		PinMap("lab/left/", labLightLeft),
+		PinMap("lab/right/", labLightRight)
+	};
+
+	int pin = -1;
+	const char *action = NULL;
+
+	for(int i = 0; i < sizeof((pins))/sizeof(PinMap); ++i) {
+		pins[i].match(param, pin, action);
+	}
+
+	if(pin < 0) {
+		send404(client);
+		return;
+	}
+
+	if(!strcmp(action, "On")) {
+		digitalWrite(pin, HIGH);
+	} else 
+	if(!strcmp(action, "Off")) {
+		digitalWrite(pin, LOW);
+	} else {
+		send404(client);
+		return;
+	}
+
+	redirect(client);
+}
+
+void sockets(EthernetClient &client, const char *param) {
+	if(param[0] >= 'A' && param[0] <= 'E' && param[1] == '/') {
+		if(!strcmp(param + 2, "On")) {
+			solight.control(31, param[0], 1);
+			redirect(client);
+		} else
+		if(!strcmp(param + 2, "Off")) {
+			solight.control(31, param[0], 0);
+			redirect(client);
+		} else
+		send404(client);
+	} else {
+		uint16_t socknum;
+		size_t i = parseInt(param, socknum);
+
+		if(!i) {
+			send404(client);
+			return;
+		}
+
+		for(; param[i] != '/' && param[i] != 0; ++i) {
+			if(param[i] < '0' || param[i] > '1') {
+				send404(client);
+				return;
+			}
+			socknum *= 10;
+			socknum += param[i] - '0';
+		}
+
+		if(!strcmp(param + i, "/On")) {
+			solight.control(socknum, 1);
+			redirect(client);
+		} else
+		if(!strcmp(param + i, "/Off")) {
+			solight.control(socknum, 0);
+			redirect(client);
+		} else {
+			send404(client);
+		}
+	}
+}
+
 StaticResource resTable[] = {
 	StaticResource("/", &sendIndex),
-	LI(mainFrontOn),
-	LI(mainFrontOff),
-	LI(mainBackOn),
-	LI(mainBackOff),
-	LI(labLeftOn),
-	LI(labLeftOff),
-	LI(labRightOn),
-	LI(labRightOff),
-	SI(AOn),
-	SI(AOff),
-	SI(BOn),
-	SI(BOff),
-	SI(COn),
-	SI(COff),
-	SI(DOn),
-	SI(DOff),
-	SI(EOn),
-	SI(EOff),
 	StaticResource("/everythingOff", &everythingOff)
+};
+
+ParamResource categories[] = {
+	ParamResource("/lights/", &lights),
+	ParamResource("/sockets/", &sockets),
+	ParamResource("/everythingOff/", &everythingOffTimer)
 };
 
 // Reads first line of HTTP request and extracts the path of max length = len
@@ -386,6 +473,9 @@ bool sendResponse(EthernetClient &client, const char *path) {
 	for(int i = 0; i < sizeof(resTable)/sizeof(StaticResource); ++i) {
 		if(resTable[i].match(path, client)) return true;
 	}
+	for(int i = 0; i < sizeof(categories)/sizeof(ParamResource); ++i) {
+		if(categories[i].match(path, client)) return true;
+	}
 	return false;
 }
 
@@ -458,5 +548,10 @@ void loop()
 			digitalWrite(labLightRight, !digitalRead(labLightRight));
 			lastLabRight = curLabRight;
 		}
+	}
+
+	if(countdownTimer && (unsigned long)(millis() - countdownStart) >= countdownMillis) {
+		turnEverythingOff();
+		countdownTimer = false;
 	}
 }
